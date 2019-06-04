496101c7abd0fbd585b2435a2d1bc24f84bc8e99
Patrick Adan <patrick.tafa@gmail.com>
2019-06-03 23:05:31 -0300

    Initial commit

---
+++ frontend/src/about/about.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import PageHeader from '../template/pageHeader'
+
+export default props => (
+    <div>
+        <PageHeader name='Sobre' small='Nós' />
+        <h2>Nossa Historia</h2>
+        <p>Lorem ipsum magna aliquet vehicula potenti dictumst amet aliquam vestibulum, pretium litora et ante nisi donec euismod odio, curabitur urna mauris a sagittis aliquet mattis tincidunt. volutpat feugiat conubia tempor metus quis mauris auctor, ante accumsan commodo curae eget et sollicitudin, platea litora</p>
+        <h2>Missao</h2>
+        <p>Lorem ipsum magna aliquet vehicula potenti dictumst amet</p>
+        <h2>Imprensa</h2>
+        <p>Lorem ipsum magna aliquet vehicula potenti dictumst amet</p>
+    </div>
+)
