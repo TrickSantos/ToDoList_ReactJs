5a73db385d76e2881969088bab6d8ce9d603f0b4
TrickSantos <patrick.tafa@gmail.com>
2019-06-04 17:27:03 -0400

    Commit do Backend

--- frontend/src/todo/todo.jsx
+++ frontend/src/todo/todo.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from "react";
+import axios from 'axios'
+
 import PageHeader from '../template/pageHeader'
 import TodoForm from './todoForm'
 import TodoList from './todoList'
+
+const URL = 'http://localhost:3003/api/todos'
 
 export default class Todo extends Component{
     constructor(props){
@@ -13,10 +17,22 @@ export default class Todo extends Component{
         }
         this.handleAdd = this.handleAdd.bind(this)
         this.handleChange = this.handleChange.bind(this)
+
+        this.refresh();
+    }
+
+    refresh(){
+        axios.get(`${URL}?sort=createdAt`)
+        .then(resp => this.setState({
+            ...this.state, description: '',
+            list: resp.data
+        }))
     }
 
     handleAdd(){
-        console.log(this.state.description);
+        const description = this.state.description
+        axios.post(URL, {description})
+        .then(resp => this.refresh())
         
     }
 
--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
 
-export default props => (
-    <div>
-        <h1>List</h1>
-    </div>
-)
+export default props => {
+
+    return (
+        <table className='table'>
+            <thead>
+                <tr>
+                    <th>Descrição</th>
+                </tr>
+            </thead>
+            <tbody>
+                {renderRows()}
+            </tbody>
+        </table>
+    )
+}
